a7dd7206241aa6b4557459cbc66060caafb78cd7
Hadar Keller <hadarkeller@gmail.com>
2025-11-20 18:34:09 +0200

    added Bootstrap integration and improved calculator functionality with input validation

--- 02_HW/index.js
+++ 02_HW/index.js
@@ -16,22 +16,64 @@ function pageLoaded() {
     btnRes.addEventListener("click", () => {
         calculate();
     });
+    // validate while typing
+    in1.addEventListener('input', () => validateNumberInput(in1));
+    in2.addEventListener('input', () => validateNumberInput(in2));
 }
 
-const btn2 = document.getElementById("btn2");
+//const btn2 = document.getElementById("btn2");
 
 
-btn2.addEventListener("click", () => {
+/* btn2.addEventListener("click", () => {
     print("btn2 clicked" + btn2.id + "|" + btn2.innerText);
-});
+}); */
 
 function calculate() {
-    let in1txt = in1.value;
-    let in2txt = in2.value;
-    let num1 = parseInt(in1txt);
-    let num2 = parseInt(in2txt);
-    let sum = num1 + num2;
-    res.innerText = sum;
+    // validate inputs and get numeric values
+    const v1 = validateNumberInput(in1);
+    const v2 = validateNumberInput(in2);
+    if (!v1.valid || !v2.valid) {
+        res.innerText = 'Please enter valid numbers';
+        return;
+    }
+    let num1 = v1.value;
+    let num2 = v2.value;
+    let operation = document.getElementById("operator").value;
+    let result;
+    switch (operation) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;      
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        default:
+            res.innerText = "Unknown operation";
+            break;
+    }
+    res.innerText = result;
+    print(num1 + " " + operation + " " + num2 + " = " + result, true);
+}
+
+function validateNumberInput(el) {
+    if (!el) return { valid: false, value: NaN };
+    const txt = el.value.trim();
+    const num = parseFloat(txt);
+    if (txt === '' || isNaN(num)) {
+        el.classList.remove('is-valid');
+        el.classList.add('is-invalid');
+        return { valid: false, value: NaN };
+    } else {
+        el.classList.remove('is-invalid');
+        el.classList.add('is-valid');
+        return { valid: true, value: num };
+    }
 }
 
 /*btn2.addEventListener("click",func1());
@@ -39,12 +81,22 @@ function calculate() {
 function func1(){   }*/
 
 
-function print(msg) {
-    //get text area element reference
+function print(msg, boo) 
+{
     const ta = document.getElementById("output");
-    //--write to text area--
-    if (ta) ta.value = msg;
-    else console.log(msg);
+    if (boo) 
+    {
+        //append to text area
+        if (ta) ta.value = msg + "\n"+ ta.value;
+            else console.log(msg);
+    }
+    else 
+        {
+        //get text area element reference
+        //--write to text area--
+        if (ta) ta.value = msg;
+            else console.log(msg);
+    }
 }
 
 
@@ -82,7 +134,7 @@ function demoNative() {
     // Callback
     function calc(a, b, fn) { return fn(a, b); }
     const result = calc(10, 20, (x, y) => x + y);
-    out += "\n[Callback] calc(10,20, x+y ) = " + result;
+    out += "\n[Callback] calc(10,20, x+y ) = " + result+"\n";
 
     print(out);
 }
